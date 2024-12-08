cb03a32daccbc568c19211685670c239599d91b1
Nick Galvez <nicholas.galvez@gmail.com>
2024-12-07 16:03:04 -0800

    Add sum_ascii and test

--- main.cpp
+++ main.cpp
@@ -7,13 +7,54 @@
 
 using namespace std;
 
+int sum_ascii(string);
+void test();
+
+// receives a single string and returns the sum of that string's character's ASCII values.
+int sum_ascii(string ascii_text)
+{
+    int ascii_int = 0;
+    for (size_t i = 0; i < ascii_text.length(); i++)
+    {
+        /* code */
+        ascii_int += (int)ascii_text[i];
+    }
+    return ascii_int;
+}
+
+void test()
+{
+    cout << "Running tests" << endl;
+    if (sum_ascii("A") != 65) {
+        cout << "Test 1 failed, A should = 65" << endl;
+    } else {
+        cout << "Test 1 Passed" << endl;
+    }
+    if (sum_ascii("AA") != 130) {
+        cout << "Test 2 failed, AA should = 130" << endl;
+    } else {
+        cout << "Test 2 Passed" << endl;
+    }
+        if (sum_ascii("AA") != 130) {
+        cout << "Test 3 failed, AA should = 130" << endl;
+    } else {
+        cout << "Test 3 Passed" << endl;
+    }
+}
+
 int main()
 {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
+    test();
+
     return 0;
 }
+
+
+
+/*
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
